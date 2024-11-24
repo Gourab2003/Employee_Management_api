69f74b578cd6662965aae92636f59b0a6d8b0f7d
Gourab2003 <roygoourab005@gmail.com>
2024-11-24 20:04:33 +0530

    add first route to get all employees

--- app.py
+++ app.py
@@ -5,3 +5,16 @@ from flask_migrate import Migrate
 db = SQLAlchemy()
 migrate =  Migrate()
 
+def create_app():
+    app = Flask(__name__)
+    
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://admin:gourab@localhost/employee_mgmt'
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+    
+    db.init_app(app)
+    migrate.init_app(app, db)
+    
+    from routes import employee_bp
+    app.register_blueprint(employee_bp, url_prefix='/api')
+    
+    return app
--- routes.py
+++ routes.py
@@ -1,18 +1,19 @@
-from flask import request, jsonify, Blueprint
-from app import app
-from models import Employee
+from flask import request, jsonify, Blueprint 
 
+employee_bp =  Blueprint('employees',__name__)
 
-employee_bp = Blueprint('blueprint',__name__)
-
-# Example route to fetch all employees
-@app.route('/employees', methods=['GET'])
+@employee_bp.route('/employees', methods=['GET'])
 def get_employees():
+    from models import Employee
     employees = Employee.query.all()
-    return jsonify([{
-        "id": emp.id,
-        "name": emp.name,
-        "department": emp.department,
-        "designation": emp.designation,
-        "salary": emp.salary
-    } for emp in employees])
+    employee_list = [
+        {
+            "id": emp.id,
+            "name": emp.name,
+            "department": emp.department,
+            "designation": emp.designation,
+            "salary": emp.salary
+        }
+        for emp in employees
+    ]
+    return jsonify(employee_list), 200
